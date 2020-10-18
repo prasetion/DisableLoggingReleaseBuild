62df7ea5cd705fea4f6bb38a37f53dc8c9458b5a
prasetion <prasetion@gmail.com>
2020-10-18 14:15:17 +0700

    init project

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ #if !UNITY_EDITOR
+             Debug.unityLogger.logEnabled = true;
+ #else
+             Debug.unityLogger.logEnabled=false;
+ #endif
+
+public class ButtonClick : MonoBehaviour
+{
+
+    public void Click(){
+        Debug.Log("button clicked");
+    }
+
+}
